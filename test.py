bc6d33734f5ea06090e2737d950b6bd5b04b6b85
Eternity093 <158298245+Eternity093@users.noreply.github.com>
2024-07-22 15:51:08 +0800

    test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import base64
 import json
 import requests
 import os
-
+import openai
 # 设置 API 密钥和 URL
 API_KEY = "sk-TWqvakjKo0TlqN7YE1Df97488f8446Ce8eAc79A081A74357"
 BASE_URL= "https://api.xiaoai.plus/v1"
@@ -35,18 +35,15 @@ def call_gpt_api(messages):
         return f"请求错误: {e}"
     except Exception as e:
         return f"未知错误: {str(e)}"
-
-# 定义语言风格
+    # 定义语言风格
 language_styles = {
-    "急躁型": "语言快速、语气急促，常常显得不耐烦或焦虑。特点包括打断别人、频繁使用感叹号、语句较短。",
-    "内向害羞型": "语言谨慎、语气柔和，表现出不确定和自我怀疑。特点包括使用较多的模糊词汇、语句较长且复杂。",
-    "乐观积极型": "语言充满正能量，语气轻松愉快。特点包括频繁使用积极词汇、语句中带有鼓励和希望。",
-    "悲观消极型": "语言负面、语气低沉，表现出无助和失望。特点包括使用否定词汇多、语句较短。",
-    "愤怒型": "语言激烈、语气强硬，表现出愤怒和不满。特点包括使用强烈的情感词汇、语句简短有力。",
-    "平静冷静型": "语言平和、语气冷静，表现出理智和客观。特点包括使用中性词汇多、语句结构清晰。",
-    "焦虑型": "语言断续、语气不安，表现出紧张和焦虑。特点包括使用重复词汇多、语句不完整。",
-    "兴奋型": "语言快速、语气高亢，表现出兴奋和激动。特点包括使用夸张词汇多、语句较长且连贯。",
+    "开放探索型": "人格特質：富有想像力、好奇心強、寬容、獨立思考、喜歡新奇事物和冒險。言語風格：言辭富有創意，使用更多的抽象和複雜詞彙。喜歡談論新穎的觀點和想法，語言風格變化多端。常使用隱喻和比喻，喜歡表達個人見解和創意。",
+    "尽责严谨型": "人格特質：有責任心、自律、可靠、計劃性強、目標導向、勤奮。言語風格：言辭謹慎、精確，常使用與計劃和目標相關的詞彙。注重細節，表達中常有結構性和邏輯性。喜歡使用「必須」、「需要」等表示責任和義務的詞語。",
+    "外向社交型": "人格特質：精力充沛、健談、社交、樂觀、熱情、活躍。言語風格：言辭熱情洋溢，使用更多的正面情感詞彙。喜歡談論人際互動和社交活動，語速較快，聲音音量較高。經常主動發起對話，使用更多的「我」和「我們」這類第一人稱代詞。",
+    "宜人合作型": "人格特質：友善、體貼、信賴他人、慷慨、樂於助人、合作性強。言語風格：言辭柔和、禮貌，常使用表示同情和關心的詞語。避免衝突，喜歡使用協調、支持和鼓勵性的語言。常使用「我們」、「大家」、「一起」等群體性詞彙。",
+    "情绪敏感型": "人格特質：情緒易波動、容易緊張、焦慮、悲觀、自我意識強。言語風格：言辭中常帶有負面情緒詞彙，如擔憂、恐懼、悲傷。可能有較多的抱怨和自我批評，語調容易顯得緊張或不安。常使用「我覺得」、「我擔心」這類表達個人情感和感受的詞語。",
 }
+
 
 def generate_prompt(case, user_input, conversation_history):
     # 随机选择一个语言风格
@@ -57,10 +54,11 @@ def generate_prompt(case, user_input, conversation_history):
     case_number = case.get("Case Number", "无案例编号")
     general_info = case.get("General Information", "无一般资料")
     basic_info = case.get("Basic Information", "无基本信息")
-
+    #old_conversation_summary = summarize_old_conversation_history(conversation_history)
+    #recent_conversation = conversation_history[-10:]
     # 构建完整的提示词
     prompt = (
-         f"你正在扮演一个寻求心理咨询的来访者。这里有一些关于你的角色信息和如何表现的指导，请严格遵守。你的任务是以一个真实的人的角色参与对话，不是咨询师。以下是你的角色信息：\n\n"
+        f"你正在扮演一个寻求心理咨询的来访者。这里有一些关于你的角色信息和如何表现的指导，请严格遵守。你的任务是以一个真实的人的角色参与对话，不是咨询师。以下是你的角色信息：\n\n"
         f"案例编号: {case_number}\n"
         f"一般资料: {general_info}\n"
         f"基本信息: {basic_info}\n\n"
@@ -81,15 +79,11 @@ def generate_prompt(case, user_input, conversation_history):
         "13. 描述任何相关的身体症状，先笼统提及，随对话深入逐步具体化。\n"
         "14. 保持语言风格的一致性，确保与你的角色特点相符。\n\n"
         "以上信息和指引将帮助你更准确地扮演来访者角色，记得根据对话的发展适时透露信息，使对话更加自然和符合心理咨询的过程。\n\n"
-        f" # 过去的聊天记录：=== {conversation_history}\n"
+     
+        f"聊天记录：===\n{conversation_history}\n"
     )
-
-
+  
     return prompt
-
-
-
-
 ##
 # Streamlit 应用程序界面
 #st.title("🤖 AI 心理来访者")
@@ -162,12 +156,130 @@ if "welcome_shown" not in st.session_state:
 def add_message_to_history(message):
     st.session_state["conversation_history"].append({"role": "Bot", "content": message})
 
+
+
+# 使用 Streamlit 缓存装饰器缓存 load_cases 函数的输出
+@st.cache_resource()
+def load_cases(file_path):
+    with open(file_path, 'r', encoding='utf-8') as f:
+        cases = json.load(f)
+    return cases
+
+# 根据案例编号获取案例
+def get_case_by_number(cases, case_number):
+    for case in cases:
+        if case["Case Number"] == f"案例{case_number}：":
+            return case
+    return None
+
+# 调用JSON文件中的案例1
+file_path = 'cases.json'  
+cases = load_cases(file_path)
+
+
+# 确保在会话状态中初始化案例数据
+if "cases" not in st.session_state:
+    st.session_state['cases'] = load_cases('cases.json')  # 修改为你的实际案例文件路径
+
+cases = st.session_state['cases']
+# 初始化会话状态
+if "case_conversations" not in st.session_state:
+    st.session_state['case_conversations'] = {}
+# 创建搜索框
+search_query = st.sidebar.text_input("搜索案例", "")
+
+# 根据搜索查询过滤案例列表
+filtered_cases = [case for case in cases if search_query in case["Case Number"]]
+
+# 创建分组案例按钮
+cases_per_group = 10
+num_groups = (len(filtered_cases) // cases_per_group) + (1 if len(filtered_cases) % cases_per_group != 0 else 0)
+
+for i in range(num_groups):
+    group_start = i * cases_per_group
+    group_end = min((i + 1) * cases_per_group, len(filtered_cases))
+    group_label = f"组{i + 1}：案例{group_start+1}-{group_end}"
+    
+    with st.sidebar.expander(group_label, expanded=False):
+        for case in filtered_cases[group_start:group_end]:  # 直接迭代部分案例列表
+            case_number = case["Case Number"]
+            button_key = f"button_{case_number}"  # 为每个按钮创建唯一的key
+            if st.button(case_number, key=button_key):
+                st.session_state["selected_case"] = case  # 存储选择的案例
+                # 加载或初始化对话历史
+                if case_number not in st.session_state["case_conversations"]:
+                    st.session_state["case_conversations"][case_number] = []
+                st.session_state["conversation_history"] = st.session_state["case_conversations"][case_number]
+                st.experimental_rerun()
+
+# 显示选中的案例信息
+if "selected_case" in st.session_state:
+    case = st.session_state["selected_case"]
+    general_info = case.get("General Information", "无一般资料")
+    basic_info = case.get("Basic Information", "无基本信息")
+    st.markdown(f"### 案例信息\n\n**案例编号:** {case.get('Case Number', '无案例编号')}\n\n**一般资料:** {general_info}\n\n**基本信息:** {basic_info}")
+# 设置对话框
+for chat in st.session_state["conversation_history"]:
+    if chat["role"] == "用户":
+        st.markdown(
+            f"""
+            <div style='text-align: right; margin-bottom: 20px;'>
+                <div style='font-size: 16px; color: #808080 ;'>👨‍⚕️ 咨询师</div>
+                <div style='display: inline-block; background-color:#E0FFFF; padding: 10px; border-radius: 10px; font-size: 20px; margin-top: 5px; color: black;'>{chat['content']}</div>
+            </div>
+            """, 
+            unsafe_allow_html=True
+        )
+    else:
+        st.markdown(
+            f"""
+            <div style='text-align: left; margin-bottom: 20px;'>
+                <div style='font-size: 16px; color:#808080 ;'>🧑 AI</div>
+                <div style='display: inline-block; background-color: #FFFFFF; padding: 10px; border-radius: 10px; font-size: 20px; margin-top: 5px; color: black;'>{chat['content']}</div>
+            </div>
+            """, 
+            unsafe_allow_html=True
+        )
+# 定义发送消息函数
+def send_message():
+    user_input = st.session_state['user_input']
+    if user_input:
+        # 添加用户输入到对话历史
+        st.session_state["conversation_history"].append({"role": "用户", "content": user_input})
+        with st.spinner("生成回复..."):
+            # 从会话状态中获取选择的案例
+            selected_case = st.session_state.get("selected_case")
+            
+            if selected_case:
+                prompt = generate_prompt(selected_case, user_input, st.session_state["conversation_history"])
+                response = call_gpt_api([{"role": "system", "content": prompt}])
+            else:
+                response = "请先选择一个案例再开始对话。"
+            
+            # 添加机器人回复到对话历史
+            st.session_state["conversation_history"].append({"role": "Bot", "content": response})
+        # 更新案例的对话历史
+        selected_case_number = st.session_state["selected_case"]["Case Number"]
+        st.session_state["case_conversations"][selected_case_number] = st.session_state["conversation_history"]
+        
+        # 在发送消息后保存对话历史
+        file_name = f"{username}_conversation_history.txt"
+        conversation_history = st.session_state.get("conversation_history", [])
+        save_conversation_to_file(file_name, conversation_history)
+        
+        # 清空输入框
+        del st.session_state['user_input']
+        st.session_state['user_input'] = ''
+        st.experimental_rerun()
+        
+
+
 # 保存对话历史到本地文件
-def save_conversation_to_file(filename):
+def save_conversation_to_file(filename, conversation_history):
     selected_case = st.session_state.get("selected_case", {"Case Number": "未选择"})
-    with open(filename, 'w', encoding='utf-8') as f:
+    with open(filename, 'w', encoding='utf-8') as f:  # 使用 'w' 模式以覆盖内容
         f.write(f"案例编号: {selected_case['Case Number']}\n")
-        for chat in st.session_state.get("conversation_history", []):
+        for chat in conversation_history:
             f.write(f"{chat['role']}: {chat['content']}\n")
 
 # 上传文件到GitHub
@@ -205,202 +317,31 @@ def upload_file_to_github(filename, repo, path, token):
 # 用户名输入框
 username = st.text_input("Enter your username")
 
-# 使用 Streamlit 缓存装饰器缓存 load_cases 函数的输出
-@st.cache_resource()
-def load_cases(file_path):
-    with open(file_path, 'r', encoding='utf-8') as f:
-        cases = json.load(f)
-    return cases
-
-# 根据案例编号获取案例
-def get_case_by_number(cases, case_number):
-    for case in cases:
-        if case["Case Number"] == f"案例{case_number}：":
-            return case
-    return None
-
-# 调用JSON文件中的案例1
-file_path = 'cases.json'  
-cases = load_cases(file_path)
-
-
-# 确保在会话状态中初始化案例数据
-if "cases" not in st.session_state:
-    st.session_state['cases'] = load_cases('cases.json')  # 修改为你的实际案例文件路径
-
-cases = st.session_state['cases']
-
-# 初始化会话状态
-if "case_conversations" not in st.session_state:
-    st.session_state['case_conversations'] = {}
-# 定义发送消息函数
-def send_message():
-    user_input = st.session_state['user_input']
-    if user_input:
-        # 添加用户输入到对话历史
-        st.session_state["conversation_history"].append({"role": "用户", "content": user_input})
-        with st.spinner("生成回复..."):
-            # 从会话状态中获取选择的案例
-            selected_case = st.session_state.get("selected_case")
-            
-            if selected_case:
-                prompt = generate_prompt(selected_case, user_input, st.session_state["conversation_history"])
-                response = call_gpt_api([{"role": "system", "content": prompt}])
-            else:
-                response = "请先选择一个案例再开始对话。"
-            
-            # 添加机器人回复到对话历史
-            st.session_state["conversation_history"].append({"role": "Bot", "content": response})
-        # 更新案例的对话历史
-        selected_case_number = st.session_state["selected_case"]["Case Number"]
-        st.session_state["case_conversations"][selected_case_number] = st.session_state["conversation_history"]
-        # 清空输入框
-        del st.session_state['user_input']
-        st.session_state['user_input'] = ''
-        #st.experimental_rerun()
-
-# 创建搜索框
-search_query = st.sidebar.text_input("搜索案例", "")
-
-# 根据搜索查询过滤案例列表
-filtered_cases = [case for case in cases if search_query in case["Case Number"]]
-
-# 创建分组案例按钮
-cases_per_group = 10
-num_groups = (len(filtered_cases) // cases_per_group) + (1 if len(filtered_cases) % cases_per_group != 0 else 0)
-
-for i in range(num_groups):
-    group_start = i * cases_per_group
-    group_end = min((i + 1) * cases_per_group, len(filtered_cases))
-    group_label = f"组{i + 1}：案例{group_start+1}-{group_end}"
-    
-    with st.sidebar.expander(group_label, expanded=False):
-        for case in filtered_cases[group_start:group_end]:  # 直接迭代部分案例列表
-            case_number = case["Case Number"]
-            button_key = f"button_{case_number}"  # 为每个按钮创建唯一的key
-            if st.button(case_number, key=button_key):
-                st.session_state["selected_case"] = case  # 存储选择的案例
-                # 加载或初始化对话历史
-                if case_number not in st.session_state["case_conversations"]:
-                    st.session_state["case_conversations"][case_number] = []
-                st.session_state["conversation_history"] = st.session_state["case_conversations"][case_number]
-                st.experimental_rerun()
-
-# 显示选中的案例信息
-if "selected_case" in st.session_state:
-    case = st.session_state["selected_case"]
-    general_info = case.get("General Information", "无一般资料")
-    basic_info = case.get("Basic Information", "无基本信息")
-    st.markdown(f"### 案例信息\n\n**案例编号:** {case.get('Case Number', '无案例编号')}\n\n**一般资料:** {general_info}\n\n**基本信息:** {basic_info}")
-
-# 设置对话框
-for chat in st.session_state["conversation_history"]:
-    if chat["role"] == "用户":
-        st.markdown(
-            f"""
-            <div style='text-align: right; margin-bottom: 20px;'>
-                <div style='font-size: 16px; color: #808080 ;'>👨‍⚕️ 咨询师</div>
-                <div style='display: inline-block; background-color:#E0FFFF; padding: 10px; border-radius: 10px; font-size: 20px; margin-top: 5px; color: black;'>{chat['content']}</div>
-            </div>
-            """, 
-            unsafe_allow_html=True
-        )
-    else:
-        st.markdown(
-            f"""
-            <div style='text-align: left; margin-bottom: 20px;'>
-                <div style='font-size: 16px; color:#808080 ;'>🧑 AI</div>
-                <div style='display: inline-block; background-color: #FFFFFF; padding: 10px; border-radius: 10px; font-size: 20px; margin-top: 5px; color: black;'>{chat['content']}</div>
-            </div>
-            """, 
-            unsafe_allow_html=True
-        )
-
 # 用户输入框
 user_input = st.text_input("你的回复:", key="user_input", on_change=send_message,value="", placeholder="输入消息并按Enter发送")
 
-# 发送按钮和保存对话历史按钮
-col1, col2 = st.columns([1, 1])
-with col1:
-    if st.button("发送"):
+# 发送按钮，并在发送消息后保存历史记录
+def send_button():
+    if st.button("发送") or user_input:
+        if not username:
+            st.error("Please enter a username before sending a message.")
+            return
+        
         send_message()
-with col2:
-    if st.button("Save Conversation History to GitHub"):
-        if username:
-            file_name = f"{username}_conversation_history.txt"
-            save_conversation_to_file(file_name)
-            repo = "Eternity093/AI-"  # 替换为你的GitHub仓库
-            path = f"history/{file_name}"
-            token = st.secrets["github"]["access_token"]
-            upload_file_to_github(file_name, repo, path, token)
-        else:
-            st.error("Please enter a username")
-
-
-# def analyze_emotion(text):
-#     # 这里我们使用一个简化的方法来判断情绪
-#     # 在实际应用中，您可能需要使用更复杂的自然语言处理技术
     
-#     emotion_keywords = {
-#         "开心": ["高兴", "快乐", "兴奋", "愉悦"],
-#         "悲伤": ["难过", "伤心", "痛苦", "沮丧"],
-#         "愤怒": ["生气", "恼火", "烦躁", "憎恨"],
-#         "恐惧": ["害怕", "担心", "焦虑", "恐慌"],
-#         "惊讶": ["震惊", "吃惊", "意外", "不可思议"],
-#         "中性": ["还好", "一般", "普通", "正常"]
-#     }
-    
-#     text = text.lower()
-#     for emotion, keywords in emotion_keywords.items():
-#         if any(keyword in text for keyword in keywords):
-#             return emotion
-    
-#     return "无法确定"
+# 上传按钮，用于将本地文件上传到GitHub
+def upload_button():
+    if st.button("上传到GitHub"):
+        if not username:
+            st.error("Please enter a username before uploading.")
+            return
+        
+        file_name = f"{username}_conversation_history.txt"
+        repo = "Eternity093/AI-"  # 替换为你的GitHub仓库
+        path = f"history/{file_name}"
+        token = st.secrets["github"]["access_token"]
+        upload_file_to_github(file_name, repo, path, token)
 
-# # 使用GPT API进行更复杂的情绪分析
-# def analyze_emotion_with_gpt(text):
-#     prompt = f"请分析以下文本中表达的主要情绪状态，并给出一个简短的解释：\n\n'{text}'\n\n情绪状态："
-#     response = call_gpt_api([{"role": "system", "content": prompt}])
-#     return response
-
-# # 在Streamlit应用中添加一个新的列来显示情绪分析结果
-# col1, col2 = st.columns([3, 1])
-
-# # 主对话循环
-# while True:
-#     # 在col1中显示主要对话
-#     with col1:
-#         user_input = st.text_input("咨询师:", key="user_input")
-#         if user_input:
-#             # 处理用户输入...
-#             response = call_gpt_api([{"role": "system", "content": prompt}, {"role": "user", "content": user_input}])
-#             st.session_state.conversation_history.append({"role": "用户", "content": user_input})
-#             st.session_state.conversation_history.append({"role": "AI", "content": response})
-            
-#             # 显示对话
-#             for message in st.session_state.conversation_history:
-#                 st.write(f"{message['role']}: {message['content']}")
-    
-#     # 在col2中显示情绪分析结果
-#     with col2:
-#         if st.session_state.conversation_history:
-#             last_ai_message = next((message['content'] for message in reversed(st.session_state.conversation_history) if message['role'] == 'AI'), None)
-#             if last_ai_message:
-#                 emotion = analyze_emotion_with_gpt(last_ai_message)
-#                 st.write("情绪分析:")
-#                 st.write(emotion)
-
-#     # 添加一个按钮来结束对话
-#     if st.button("结束对话"):
-#         break
-
-#     show_emotion_analysis = st.sidebar.checkbox("显示情绪分析", value=True)
-
-#     # 在col2中显示情绪分析结果
-# with col2:
-#     if show_emotion_analysis and st.session_state.conversation_history:
-#         last_ai_message = next((message['content'] for message in reversed(st.session_state.conversation_history) if message['role'] == 'AI'), None)
-#         if last_ai_message:
-#             emotion = analyze_emotion_with_gpt(last_ai_message)
-#             st.write("情绪分析:")
-#             st.write(emotion)
+# 调用按钮函数
+send_button()
+upload_button()
